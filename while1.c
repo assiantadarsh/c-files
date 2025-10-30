e8b4da686e001ef81cd37c235d9869d3a6a33a7c
assiantadarsh <adarshbhai917963@gmail.com>
2025-10-31 00:41:35 +0530

    Add files via upload

---
+++ while1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+//PROBLEM1: WAP using a while loop to print number 1 to n//
+int i =1;
+int n;
+printf("enter n :");
+scanf("%d",&n);
+while(i<=n)
+    {printf("hii %d\n",i);
+    i++;}
+
+    return 0;
+}
