f9e7fdf930243ae5ff5b1310ec895bf6fefafbb5
assiantadarsh <adarshbhai917963@gmail.com>
2025-11-22 18:08:22 +0530

    Add files via upload

---
+++ star pyramid.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+/*
+       *
+     * * *
+   * * * * *
+ * * * * * * *
+print the given pattern ?
+*/
+int n ;
+printf("enter number of lines:");
+scanf("%d",&n);
+for(int i=1;i<=n;i++){
+    if(i%2 !=0)
+        {for(int j=1;j<=n-i;j++){
+            printf(" ");
+        }
+        for(int k=1;k<=i;k++){
+                printf(" *");
+        }
+    printf("\n");}
+}
+return 0;
+}
