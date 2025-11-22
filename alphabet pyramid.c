7e963c4ed55515eab1d2b99e176e55356eb8136e
assiantadarsh <adarshbhai917963@gmail.com>
2025-11-22 19:11:09 +0530

    Add files via upload

---
+++ alphabet pyramid.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+/*
+       A
+     A B C
+   A B C D E
+ A B C D E F G
+print the given pattern ?
+*/
+int n ;
+printf("enter number of lines:");
+scanf("%d",&n);
+for(int i=1;i<=n;i++)
+{   int a = 1;
+    if(i%2 !=0)
+        {for(int j=1;j<=n-i;j++){
+            printf(" ");
+        }
+        for(int k=1;k<=i;k++){
+            int d = a+64;
+            char ch=(char)d;
+            printf(" %c",ch);
+            a++;
+        }
+    printf("\n");}
+}
+return 0;
+}
