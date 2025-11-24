12679cbb64400baef532efe0f9d81929bdb4cc83
assiantadarsh <adarshbhai917963@gmail.com>
2025-11-24 22:26:39 +0530

    Add files via upload

---
+++ problem3 array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+/*Count the number of elements in given array 
+greator than a given number x ?
+ solve the given problem .
+*/
+int arr[5] = {-100,-30,-90,-50,-456789};
+int x,count =0;
+printf("enter any number :");
+scanf("%d",&x);
+for(int i=0;i<=4;i++){
+    if(x<=arr[i]){
+        count = count +1;
+    }
+}
+printf("the number is greator than x : %d",count);
+
+return 0;
+}
