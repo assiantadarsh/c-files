058f0d5aca1d1801a82a85a5329107bb243460d7
assiantadarsh <adarshbhai917963@gmail.com>
2025-12-02 23:23:47 +0530

    Add files via upload

---
+++ second largest array.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+/*#include<limits.h>
+int main(){
+
+//find the second largest elament in the array 
+
+int arr[5] = {3,5,6,4,3};
+int max=INT_MIN;
+int smax=INT_MIN;
+for(int i=0;i<=4;i++){
+    if(max<arr[i]){
+        max = arr[i];
+    }
+}
+for(int i=0;i<=4;i++){
+    if(smax<arr[i] && arr[i] != max){
+        smax = arr[i];
+    }
+}
+printf("%d",smax);
+return 0;
+}*/
+//method 2
+int main(){
+int arr[6] ={-4,-8,-9,-5,2,-10};
+int max = arr[0],smax;
+for(int i=0;i<=5;i++){
+    if(max<arr[i]){
+        smax = max;
+        max = arr[i];
+    }
+}
+printf("%d",smax);
+return 0;
+}
+
+
+
+
+
+
+
