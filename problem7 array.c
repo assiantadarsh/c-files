1661aab3ae8bca67557ad16a36b9f02a42dfd95b
assiantadarsh <adarshbhai917963@gmail.com>
2025-12-03 13:05:37 +0530

    Update problem7 array.c

--- problem7 array.c
+++ problem7 array.c
@@ -6,9 +6,10 @@ the reverse order .
 */
 int arr[5] = {2,4,5,6,7};
 int brr[5];
-for(int i=4;i>=0;i--){
-    brr[i]=arr[i];
-    printf("%d ",brr[i]);
-}
+for(int i=0;i<=4;i++){
+    brr[i]=arr[4-i];}
+for(int i=0;i<=4;i++){
+    printf("%d ",brr[i]);}
 return 0;
+
 }
