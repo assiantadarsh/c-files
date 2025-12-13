6f0f63c9405d5de123e30c4e81dea847bda6b513
assiantadarsh <adarshbhai917963@gmail.com>
2025-12-13 22:43:25 +0530

    Add files via upload

--- Leetcode prob867.c
+++ Leetcode prob867.c
@@ -14,12 +14,14 @@ for(int i=0;i<r;i++){
     }
 }
 printf("\n");
+printf("original matrix\n");
 for(int i=0;i<r;i++){
     for(int j=0;j<c;j++){
         printf("%d ",arr[i][j]);
     }
     printf("\n");
 }
+printf("transpose of matrix\n");
 for(int i=0;i<c;i++){
     for(int j=0;j<r;j++){
         printf("%d ",arr[j][i]);
