56bca02d07e0d81cd9f039b820d8f15811e59172
assiantadarsh <adarshbhai917963@gmail.com>
2026-01-03 23:50:55 +0530

    Add files via upload

---
+++ basic pointers.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+int a=5;
+int* b= &a;
+printf("%p\n",&a); // address of a
+printf("%p\n",b);  // address of a
+printf("%p\n",&b); // address of b
+printf("%d\n",*b); // value of a
+return 0;
+}
